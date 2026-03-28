29f4b02be17bbb68db381faee6ba9e4c6b2d3d94
RustyYato <rustyyato@gmail.com>
2026-03-28 15:07:37 -0500

    implement decode for sigma

--- LeanMath/Data/Fintype/Sigma.lean
+++ LeanMath/Data/Fintype/Sigma.lean
@@ -5,52 +5,43 @@ import LeanMath.Data.Nat.Basic
 namespace Sigma
 
 private def all (n: ℕ) (card: Fin n -> ℕ) :
-  Fin (∑i, card i) ↭ Σi, Fin (card i) :=
+  Fin (∑i, card i) ≃ Σi, Fin (card i) :=
   match n with
   | 0 => {
     toFun := nofun
-    inj' := nofun
-    surj' := nofun
+    invFun := nofun
+    leftInv := nofun
+    rightInv := nofun
   }
   | n + 1 =>
     have allsucc := all n (card ∘ Fin.succ)
     have eqv := (Fin.sum (card 0) (∑i: Fin n, card i.succ)).comp
       <| Equiv.fin_cast (n := ∑i, card i) <| by rw [fin_sum_succ]
-    eqv.toBij.trans <| {
+    eqv.trans <| {
       toFun
       | .inl x => ⟨0, x⟩
       | .inr x =>
         have ⟨i, x⟩ := allsucc x
         ⟨i.succ, x⟩
-      inj' := by
-        intro a b h; dsimp at h
-        cases a <;> cases b <;> (dsimp at h)
-        congr
-        replace h := Sigma.mk.inj h
-        exact eq_of_heq h.2
-        replace h := Sigma.mk.inj h
-        have := Fin.succ_ne_zero _ h.1.symm
-        contradiction
-        replace h := Sigma.mk.inj h
-        have := Fin.succ_ne_zero _ h.1
-        contradiction
-        congr
-        rename_i a b
-        refine @allsucc.inj _ _ a b ?_
-        simp at h
-        ext1
-        exact h.left
-        exact h.right
-      surj' := by
-        intro ⟨a, b⟩
-        cases a using Fin.cases
-        exists .inl b
-        rename_i a
-        have ⟨i, hi⟩ := allsucc.surj ⟨a, b⟩
-        exists .inr i
-        dsimp;
-        have ⟨_, _⟩ := Sigma.mk.inj hi
-        congr
+      invFun
+      | ⟨⟨0, _⟩, x⟩ => .inl x
+      | ⟨⟨i + 1, hi⟩, x⟩ => .inr (allsucc.symm ⟨⟨i, Nat.lt_of_succ_lt_succ hi⟩, x⟩)
+      leftInv x := by
+        obtain ⟨i, x⟩ := x
+        cases i using Fin.cases with
+        | zero => rfl
+        | succ i =>
+          dsimp [Fin.succ]
+          show Sigma.mk (Fin.mk (Fin.val (allsucc (allsucc.symm ⟨i, x⟩)).fst + 1) _) _ = _
+          congr
+          rw [Equiv.symm_coe]
+          show HEq (allsucc (allsucc.symm ⟨i, x⟩)).snd _
+          rw [Equiv.symm_coe]
+      rightInv x := by
+        rcases x with x | x
+        dsimp; rfl
+        show Sum.inr (allsucc.symm (allsucc x)) = _
+        simp
     }
 
 end Sigma
@@ -62,33 +53,85 @@ section
 private def instSigma' (card: Fin n -> ℕ) : Fintype (Σi, Fin (card i)) where
   card := ∑i, card i
   repr := Trunc.mk {
-    bij := Sigma.all _ card
-    try_decode := .none
+    bij := (Sigma.all _ card).toBij
+    try_decode := .some {
+      val := (Sigma.all _ card).symm
+      property x := by
+        dsimp; rw [Equiv.coe_symm]
+    }
   }
+
+private def bijOrEquiv : (α ↭ β) ⊕' (α ≃ β) -> (α ↭ β)
+| .inl x => x
+| .inr x => x.toBij
 
 variable {ι: Sort*} {α: ι -> Sort*} [ft: Fintype ι] [fα: ∀i, Fintype (α i)]
 
+private def factor
+  (rι: (Fin (Fintype.card ι) ↭ ι) ⊕' (Fin (Fintype.card ι) ≃ ι))
+  (rα: (∀i, Fin (Fintype.card (α (bijOrEquiv rι i))) ↭ α (bijOrEquiv rι i)) ⊕' (∀i, Fin (Fintype.card (α (bijOrEquiv rι i))) ≃ α (bijOrEquiv rι i)))
+: (
+    Σ' (rι': Fin (Fintype.card ι) ↭ ι), ∀i, Fin (Fintype.card <| α (rι' i)) ↭ α (rι' i)
+  ) ⊕' (
+    Σ' (rι': Fin (Fintype.card ι) ≃ ι), ∀i, Fin (Fintype.card <| α (rι' i)) ≃ α (rι' i)
+  ) := match rι, rα with
+    | .inr rι, .inr rα => .inr ⟨rι, rα⟩
+    | .inl rι, .inl rα => .inl ⟨rι, rα⟩
+    | .inr rι, .inl rα => .inl ⟨rι.toBij, rα⟩
+    | .inl rι, .inr rα => .inl ⟨rι, fun i => (rα i).toBij⟩
+
+private def fin_heq (h: n = m) (x: Fin n) (y: Fin m) (g: x = y.cast h.symm) : HEq x y := by
+  subst h; simp [g]
+
 instance : Fintype (Σ'i, α i) :=
-  (finBij ι).recOnSubsingleton fun rι =>
-  (finTruncChoice (fun i => finBij (α (rι i)))).recOnSubsingleton fun rα => by
-  have := @instSigma'
-  apply ofBij (α := Σi: Fin (card ι), Fin (card (α (rι i))))
-  exact {
-    toFun a := ⟨rι a.1,  rα _ a.2⟩
-    inj' := by
-      intro ⟨a₀, a₁⟩ ⟨b₀, b₁⟩ h
-      dsimp at h
-      have ⟨h₀, h₁⟩ := (PSigma.mk.inj h)
-      cases rι.inj h₀
-      have := (rα a₀).inj (eq_of_heq h₁)
-      congr
-    surj' := by
-      intro ⟨a, b⟩
-      obtain ⟨a, rfl⟩ := rι.surj a
-      obtain ⟨b, h⟩ := (rα _).surj b
-      exists ⟨a, b⟩
-      dsimp; congr
-  }
+  (finBijOrEquiv ι).recOnSubsingleton fun rι =>
+  (finTruncChoice (fun i => finBijOrEquiv (α (bijOrEquiv rι i)))).recOnSubsingleton fun rα => by
+  have := factor rι (factorBijOrEquiv rα)
+  clear rι rα
+  match this with
+  | .inl ⟨rι, rα⟩ =>
+    have := @instSigma'
+    apply ofBij (α := Σi: Fin (card ι), Fin (card (α (rι i))))
+    exact {
+      toFun a := ⟨rι a.1,  rα _ a.2⟩
+      inj' := by
+        intro ⟨a₀, a₁⟩ ⟨b₀, b₁⟩ h
+        dsimp at h
+        have ⟨h₀, h₁⟩ := (PSigma.mk.inj h)
+        cases rι.inj h₀
+        have := (rα a₀).inj (eq_of_heq h₁)
+        congr
+      surj' := by
+        intro ⟨a, b⟩
+        obtain ⟨a, rfl⟩ := rι.surj a
+        obtain ⟨b, h⟩ := (rα _).surj b
+        exists ⟨a, b⟩
+        dsimp; congr
+    }
+  | .inr ⟨rι, rα⟩ =>
+    have := @instSigma'
+    apply ofEquiv (α := Σi: Fin (card ι), Fin (card (α (rι i))))
+    exact {
+      toFun a := ⟨rι a.1,  rα _ a.2⟩
+      invFun a := ⟨rι.symm a.1,  (rα _).symm (cast (by congr; rw [Equiv.symm_coe rι]) a.2)⟩
+      leftInv x := by
+        obtain ⟨x, ty⟩ := x
+        simp
+      rightInv x := by
+        obtain ⟨x, ty⟩ := x
+        simp
+        apply fin_heq
+        apply inj (rα (rι.symm (rι x)))
+        simp; apply eq_of_heq; apply (cast_heq _ _).trans
+        congr; any_goals simp
+        apply Function.hfunext
+        congr <;> simp
+        intro i hi hi'
+        rfl
+        apply fin_heq
+        rfl
+        any_goals congr <;> simp
+    }
 
 end
 
@@ -106,13 +149,6 @@ end Fintype
 namespace Finite
 
 section
-
-private def instSigma' (card: Fin n -> ℕ) : Fintype (Σi, Fin (card i)) where
-  card := ∑i, card i
-  repr := Trunc.mk {
-    bij := Sigma.all _ card
-    try_decode := .none
-  }
 
 variable {ι: Sort*} {α: ι -> Sort*} [ft: Finite ι] [fα: ∀i, Finite (α i)]
 
