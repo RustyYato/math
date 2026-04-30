956d73e1c7c2d65bf3befc038160199c2409ec73
RustyYato <rustyyato@gmail.com>
2026-04-30 18:59:23 -0500

    prove that ENat.card is equal to Fintype card prove that ENat.card equals infinity of not finite

--- LeanMath/Data/ENat/Fintype.lean
+++ LeanMath/Data/ENat/Fintype.lean
@@ -1,4 +1,24 @@
 import LeanMath.Data.Fintype.Defs
 import LeanMath.Data.ENat.Defs
 
--- def ENat.card
+namespace ENat
+
+open ENat
+
+def card_eq_fintype_card (α: Sort u) [LEM] [Fintype α] : ENat.card α = Fintype.card α := by
+  open UniqueChoice in
+  unfold ENat.card
+  induction Fintype.finEquiv α with | _ eqv =>
+  rw [dif_pos]; congr
+  apply Classical.choose_unique_spec_unique
+  exact ⟨eqv⟩
+  exists Fintype.card α
+  exact ⟨eqv⟩
+
+def card_eq_inf_of_not_finite (α: Sort u) (h: ¬Finite α) [LEM] : ENat.card α = ∞ := by
+  unfold card; rw [dif_neg]
+  intro ⟨n, ⟨eqv⟩⟩
+  apply h
+  exact Finite.ofBij eqv.toBij
+
+end ENat
